c1d4a3a9bf27291d25b982db58e28a1b19358bf9
Георгий Цырков <g@tias.pro>
2023-11-03 12:07:24 +0300

    ...

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Reactive;
 using Avalonia.Interactivity;
 using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
+using ReactiveUI;
 using TEC_avalonia_2023.Models;
 
 namespace TEC_avalonia_2023.ViewModels;
@@ -13,46 +15,55 @@ public class MainWindowViewModel : ViewModelBase
 {
     public string Greeting => "Welcome to Avalonia!";
 
-    public ObservableCollection<PersonRecord> Items { get; }
+    public ReactiveCommand<Unit, Unit> ClickCommand { get; }
 
-        public MainWindowViewModel()
+    private ObservableCollection<PersonRecord> _items;
+    public ObservableCollection<PersonRecord> Items
+    {
+        get { return _items; }
+        //set { SetProperty(ref _items, value); }
+        set { _items = value; }
+    }
+
+    public MainWindowViewModel()
+    {
+        Items = new ObservableCollection<PersonRecord>();
+        ClickCommand = ReactiveCommand.Create(ClickHandler);
+    }
+
+    public void ClickHandler()
+    {
+        Items.Clear();
+
+        IWorkbook workbook;
+        using (FileStream fileStream = new FileStream("data.xlsx", FileMode.Open, FileAccess.Read))
         {
-            Items = new ObservableCollection<PersonRecord>();
+            workbook = new XSSFWorkbook(fileStream);
         }
 
-        public void ClickHandler(object sender, RoutedEventArgs args)
+        ISheet sheet = workbook.GetSheetAt(0);
+
+        int rowIndex = 4;
+
+        IRow row = sheet.GetRow(rowIndex);
+        string cellValue = row.GetCell(1).StringCellValue;
+
+        Items.Add(new PersonRecord() { Name = cellValue });
+
+        while (cellValue != string.Empty)
         {
-            Items.Clear();
-
-            IWorkbook workbook;
-            using (FileStream fileStream = new FileStream("data.xlsx", FileMode.Open, FileAccess.Read))
+            rowIndex++;
+            row = sheet.GetRow(rowIndex);
+            try
             {
-                workbook = new XSSFWorkbook(fileStream);
+                cellValue = row.GetCell(1).StringCellValue;
+                Items.Add(new PersonRecord() { Name = cellValue });
             }
-
-            ISheet sheet = workbook.GetSheetAt(0);
-
-            int rowIndex = 4;
-
-            IRow row = sheet.GetRow(rowIndex);
-            string cellValue = row.GetCell(1).StringCellValue;
-
-            Items.Add(new PersonRecord() { Name = cellValue });
-
-            while (cellValue != string.Empty)
+            catch (Exception ex)
             {
-                rowIndex++;
-                row = sheet.GetRow(rowIndex);
-                try
-                {
-                    cellValue = row.GetCell(1).StringCellValue;
-                    Items.Add(new PersonRecord() { Name = cellValue });
-                }
-                catch (Exception ex)
-                {
-                    cellValue = string.Empty;
-                }
+                cellValue = string.Empty;
             }
-
         }
+
+    }
 }

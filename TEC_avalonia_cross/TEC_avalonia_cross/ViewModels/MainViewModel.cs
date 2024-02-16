283b6b7d5af340ebb4192f32c6cedd48050e5cb7
Георгий Цырков <g@tias.pro>
2024-02-16 16:32:13 +0300

    ...

--- TEC_avalonia_cross/TEC_avalonia_cross/ViewModels/MainViewModel.cs
+++ TEC_avalonia_cross/TEC_avalonia_cross/ViewModels/MainViewModel.cs
@@ -7,6 +7,8 @@ using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
 using TEC_avalonia_cross.Models;
 using System.Reactive;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace TEC_avalonia_cross.ViewModels;
 
@@ -27,9 +29,37 @@ public class MainViewModel : ViewModelBase
         ClickCommand = ReactiveCommand.Create(ClickHandler);
     }
 
-    public void ClickHandler()
+    public async void ClickHandler()
     {
         Items.Clear();
 
+        HttpClient client = new HttpClient();
+        HttpResponseMessage response = 
+            await client.GetAsync("https://api.open-meteo.com/v1/forecast?latitude=55.7522&longitude=37.6156&hourly=temperature_2m,relative_humidity_2m,precipitation_probability&timezone=Europe%2FMoscow");
+        response.EnsureSuccessStatusCode();
+        string responseBody = await response.Content.ReadAsStringAsync();
+
+        WeatherData data = new WeatherData();
+        try
+        { 
+            data = JsonConvert.DeserializeObject<WeatherData>(responseBody);
+        }
+        catch(Exception ex)
+        {
+            data.hourly.time.Add("ERROR");
+            data.hourly.temperature_2m.Add(0);
+            data.hourly.relative_humidity_2m.Add(0);
+            data.hourly.precipitation_probability.Add(0);
+        }
+
+        for(int t = 0; t < data.hourly.time.Count; t++)
+        {
+            WeatherRecord rec = new WeatherRecord();
+            rec.Time = data.hourly.time[t];
+            if (data.hourly.temperature_2m.Count > t) { rec.Temp = data.hourly.temperature_2m[t]; }
+            if (data.hourly.relative_humidity_2m.Count > t) { rec.Humidity = (int)data.hourly.relative_humidity_2m[t]; }
+            if (data.hourly.precipitation_probability.Count > t) { rec.Precipitation = (int)data.hourly.precipitation_probability[t]; }
+            Items.Add(rec);
+        }
     }
 }
